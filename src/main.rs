d2ce9e11df4589aae80b0ce507acbfb957a51e2d
drdubel <antek.suszko@gmail.com>
2026-05-18 14:29:34 +0200

    Implement file reading functionality to display words from a specified text file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(words) = read_lines("words/sth.txt") {
+        for word in words {
+            if let Ok(w) = word {
+                println!("{}", w);
+            }
+        }
+    }
 }
